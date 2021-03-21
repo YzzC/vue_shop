fe321c0aac2959c3418fe9a073a52bd0c7eb7405
Yunzhu <yunzhu@Yunzhus-MacBook-Pro.local>
2021-03-21 00:05:06 -0400

    Finish reports

---
+++ src/components/report/Report.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <!-- 面包屑导航区 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">Home</el-breadcrumb-item>
+            <el-breadcrumb-item>Statistic</el-breadcrumb-item>
+            <el-breadcrumb-item>Reports</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片视图 -->
+        <el-card>
+            <!-- 2. prepare a DOM container with width and height -->
+            <div id="main" style="width: 600px;height:400px;"></div>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    //1.导入echart
+    import * as echarts from 'echarts'
+    import _ from 'lodash'
+
+    export default {
+        data() {
+            return {
+                // 需要合并的数据
+                options: {
+                    title: {
+                        text: '用户来源'
+                    },
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'cross',
+                            label: {
+                                backgroundColor: '#E9EEF3'
+                            }
+                        }
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: [
+                        {
+                            boundaryGap: false
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value'
+                        }
+                    ]
+                }
+            }
+        },
+        created() { },
+        //此时页面上的元素已经被渲染完毕了
+        async mounted() {
+            // 3. based on prepared DOM, initialize echarts instance
+            var myChart = echarts.init(document.getElementById('main'));
+
+            const { data: res } = await this.$http.get(`reports/type/1`)
+            if (res.meta.status !== 200) {
+                return this.$message, error('Fail to get chart')
+            }
+
+            //4.准备数据和配置项
+            const result = _.merge(res.data, this.options)
+
+            //5.展示数据
+
+            myChart.setOption(result);
+        },
+        methods: {},
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
